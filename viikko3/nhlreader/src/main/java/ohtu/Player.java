8ea8bf283bd3f47fef18954134eef16eb342fa09
Tommi <tommikokk@gmail.com>
2021-02-09 16:16:03 +0200

    exercise 1

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,40 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String team;
+    private String goals;
+    private String assists;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setGoals(String goals) {
+        this.goals = goals;
+    }
+
+    public void setAssits(String assists) {
+        this.assists = assists;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + team + " G: " + goals + " A: " + assists ;
+    }
+      
+}
